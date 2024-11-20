0d4e01961c8aa770923b1212ed92689dc493ebf8
Bruno Coutinho <brunoroockin@gmail.com>
2024-11-20 12:16:25 -0300

    Body max index

---
+++ imc.py
@@ -0,0 +1,14 @@
+name = 'Bruno'
+height = 1.90
+weight = 90
+bmi = weight / (height * height)
+
+message = '{name} is {height:.2f} meters tall, weighs {weight} kilograms, and has a BMI of {bmi:.2f}'
+formatted_message = message.format(
+    name=name, 
+    height=height, 
+    weight=weight, 
+    bmi=bmi
+)
+
+print(formatted_message)
